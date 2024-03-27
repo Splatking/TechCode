0f681791ad13134a0328b6f5a684b09516a5af3c
Splatking <523553@student.fontys.nl>
2024-03-27 11:14:54 +0100

    updating database connection

--- src/components/Scripts/DatabaseConnection.js
+++ src/components/Scripts/DatabaseConnection.js
@@ -1,43 +1,47 @@
-const express = require('express');
-const mysql = require('mysql');
+import express from 'express';
+import bcrypt from 'bcrypt';
+import { query } from 'mysql';
 
 const app = express();
 const port = 3000;
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'TechCode_Systems',
-  password: 'System123',
-  database: 'techcode_database',
-  port: 3306
-});
+app.use(express.json());
 
 function SendData(){
-  app.get('/data', (req, res) => {
-    db.query('SELECT * FROM your_table', (err, results) => {
-      if (err) {
-        console.error('Error querying database:', err);
-        res.status(500).json({ error: 'Error querying database' });
-        return;
-      }
+  app.get('/data', async (req, res) => {
+    try {
+      const results = await query('SELECT * FROM your_table');
       res.json(results);
-    });
+    } catch (err) {
+      console.error('Error querying database:', err);
+      res.status(500).json({ error: 'Error querying database' });
+    }
   });
 }
 
-function GetData(){
-  app.get('/data', (req, res) => {
-    db.query('SELECT * FROM your_table', (err, results) => {
-      if (err) {
-        console.error('Error querying database:', err);
-        res.status(500).json({ error: 'Error querying database' });
-        return;
+function LoadData(Username, Password){
+  app.post('/fetch-data', async (req, res) => {
+    try {
+      let HashedPassword = bcrypt.hash(Password);
+      console.log(HashedPassword);
+      const results = await query(`SELECT * FROM accounts WHERE Gebruikersnaam = ${Username} AND Wachtwoord = ${HashedPassword}`);
+      if (results.length > 0) {
+        const username = results[0].TechName;
+        res.json({ TechName: username });
+      } else {
+        res.status(404).json({ error: 'No data found' });
       }
-      res.json(results);
-    });
+    } catch (error) {
+      console.error('There was a problem with the database query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
   });
 }
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
+
+export { app };
+export { SendData };
+export { LoadData };
